44b1dce72f098d7b589fec0b3ee22abf77d608f5
Sam Turner <sam.turner@kokadjo.net>
2025-06-20 16:06:05 -0400

    validating data preserving state using form actions

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -12,8 +12,8 @@ export default function Signup() {
     const confirmPassword = formData.get('confirm-password');
     const role = formData.get('role');
     const terms = formData.get('terms');
-    const aquisitionChannel = formData.getAll('aquisition');
-
+    const acquisitionChannel = formData.getAll('acquisition');
+    console.log('consts', role); 
     let errors = [];
 
     if (!isEmail(email)) {
@@ -41,15 +41,29 @@ export default function Signup() {
       errors.push('You must agree to the terms and conditions.');
     }
 
-    if (aquisitionChannel.length === 0) {
-      errors.push('please select at least one aquisition channel');
+    if (acquisitionChannel.length === 0) {
+      errors.push('please select at least one acquisition channel');
     }
 
     if (errors.length > 0) {
-      return { errors: errors }
+      console.log('errors', role); 
+      return {
+        
+        errors: errors, enteredValues: {
+          email,
+          password,
+          firstName,
+          lastName,
+          confirmPassword,
+          role,
+          terms,
+          acquisitionChannel
+        }
+      }
     }
 
     return { errors: null }
+    
   }
 
   const [formState, formAction] = useActionState(signupAction, { errors: null })
@@ -61,13 +75,13 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" defaultValue={formState.enteredValues?.email} />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" defaultValue={formState.enteredValues?.password} />
         </div>
 
         <div className="control">
@@ -76,6 +90,7 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            defaultValue={formState.enteredValues?.confirmPassword}
           />
         </div>
       </div>
@@ -85,18 +100,24 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name" defaultValue={formState.enteredValues?.firstName} />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" defaultValue={formState.enteredValues?.lastName} />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select
+          id="role"
+          name="role"
+          defaultValue={formState.enteredValues?.role}
+
+        
+        >
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -113,6 +134,7 @@ export default function Signup() {
             id="google"
             name="acquisition"
             value="google"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('google')}
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -123,20 +145,30 @@ export default function Signup() {
             id="friend"
             name="acquisition"
             value="friend"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('friend')}
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input
+            type="checkbox"
+            id="other"
+            name="acquisition"
+            value="other"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('other')}
+          />
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
+          <input type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            defaultChecked={formState.enteredValues?.terms}
+          />I agree to the terms and conditions
         </label>
       </div>
       {
